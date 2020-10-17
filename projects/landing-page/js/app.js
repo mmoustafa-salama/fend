64ff9740bdd771b9fb75058a41d93afc501a65bf
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-10-17 21:30:02 +0200

    Build nav menu dynamically based on sections in landing page

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,7 +17,8 @@
  * Define Global Variables
  * 
 */
-
+const sectionList = document.querySelectorAll('section');
+const navMenuList = document.querySelector('#navbar__list');
 
 /**
  * End Global Variables
@@ -25,7 +26,26 @@
  * 
 */
 
+function createAnchorElement(textContent, cssClass) {
+    const anchor = document.createElement('a');
+    anchor.textContent = textContent;
+    anchor.classList.add(cssClass);
 
+    return anchor;
+}
+
+function clearAllActiveSectionClasses() {
+    sectionList.forEach(section => {
+        section.classList.remove('section__active');
+    });
+}
+
+function clearAllActiveLinkClasses() {
+    const navAnchors = document.querySelectorAll('#navbar__list a');
+    navAnchors.forEach(anchor => {
+        anchor.classList.remove('menu__link_active');
+    });
+}
 
 /**
  * End Helper Functions
@@ -34,24 +54,65 @@
 */
 
 // build the nav
+function buildNavMenu() {
+    const menuDocFragment = document.createDocumentFragment();
+    sectionList.forEach(section => {
+        const anchor = createAnchorElement(section.getAttribute('data-nav'), 'menu__link');
+        anchor.setAttribute('section-id', section.id);
+        anchor.addEventListener('click', scrollToSectionFn);
 
+        const menuItem = document.createElement('li');
+        menuItem.appendChild(anchor);
 
-// Add class 'active' to section when near top of viewport
+        menuDocFragment.appendChild(menuItem);
+    });
+    navMenuList.appendChild(menuDocFragment);
+}
 
+// Add class 'section__active' to section when near top of viewport
+function setActiveSection(event) {
+    sectionList.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= navMenuList.offsetHeight && rect.top > navMenuList.offsetHeight - rect.height) {
+            section.classList.add('section__active');
+        }
+    });
+}
+
+function setActiveLink() {
+    const activeSection = document.querySelector('.section__active');
+    if (activeSection) {
+        const menuAnchors = document.querySelectorAll('#navbar__list a');
+        menuAnchors.forEach(anchor => {
+            // Set active link of the section active in viewport
+            if (anchor.textContent === activeSection.getAttribute('data-nav')) {
+                anchor.classList.add('menu__link_active');
+            }
+        });
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
-
+const scrollToSectionFn = function scrollToSection(event) {
+    document.getElementById(event.target.getAttribute('section-id')).scrollIntoView({ behavior: "smooth" });
+};
 
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
+document.addEventListener('DOMContentLoaded', buildNavMenu());
 
 // Scroll to section on link click
 
 // Set sections as active
-
+document.addEventListener('scroll', () => {
+    clearAllActiveSectionClasses();
+    clearAllActiveLinkClasses();
+    setActiveSection();
+    setActiveLink();
+});
 

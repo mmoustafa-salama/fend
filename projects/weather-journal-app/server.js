42bf97975da92da4e23344e6ce9aeb0ad20713b2
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-10-28 20:25:14 +0200

    Integrate with openweathermap to fetch weather data

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,57 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.get("/data", (req, res) => {
+    res.status(200).send(projectData);
+});
+
+app.post("/data", (req, res) => {
+    const temperature = req.body.temperature;
+    const date = req.body.date;
+    const user_response = req.body.user_response;
+
+    // Check temperature is valid
+    if (!temperature) {
+        return res.status(400).send({ message: 'Temperature is required' });
+    }
+
+    // Check date is valid
+    if (!date) {
+        return res.status(400).send({ message: 'Date is required' });
+    }
+
+    projectData = {
+        temperature: temperature,
+        date: date,
+        user_response: user_response
+    };
+
+    res.status(201).send(projectData);
+});
 
 // Setup Server
+const port = 3000;
+app.listen(port, listening);
+
+function listening() {
+    console.log("Server is running on port " + port);
+    console.log('Press CTRL+C to stop server');
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,66 @@
 /* Global Variables */
+const apiKey = '50843be927de3e4a9f321bb7ccf7f92e';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+document.getElementById('generate').addEventListener('click', generateEventHandler);
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Click event handler for Generate button
+function generateEventHandler() {
+    const zip = document.getElementById('zip');
+    const feelings = document.getElementById('feelings');
+
+    getWeatherData(zip.value).then((data) => {
+        const weatherData = {
+            temperature: data.main.temp,
+            date: newDate,
+            user_response: feelings.value
+        };
+        saveWeatherData('/data', weatherData).then((data) => {
+            loadWeatherData();
+        });
+    });
+}
+
+async function getWeatherData(zipCode) {
+    if (!zipCode) zipCode = '94040'; // Set default value to USA
+    const apiUrl = `${baseUrl}?zip=${zipCode}&appid=${apiKey}`;
+    const res = await fetch(apiUrl);
+    try {
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching weather data: ' + error.message);
+    }
+}
+
+async function saveWeatherData(url, data) {
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        return await res.json();
+    } catch (error) {
+        console.error('Error saving weather data: ' + error.message);
+    }
+}
+
+async function loadWeatherData() {
+    const res = await fetch('/data');
+    try {
+        const projectData = await res.json();
+        if (projectData) {
+            document.getElementById('date').innerText = projectData.date;
+            document.getElementById('temp').innerText = projectData.temperature;
+            document.getElementById('content').innerText = projectData.user_response;
+        }
+    } catch (error) {
+        console.error('Error loading weather data: ' + error.message);
+    }
+}
